f229423c561b40d63c39895ddaa6c1280ce857f5
Catalin Bordan <catalinbordan@gmail.com>
2022-04-19 22:47:11 +0300

    minor changes-repairs
    repair main.cpp
    change the name on mqtt sensors
    change the topic on commands

--- sources/inverter-mqtt/mqtt-init.sh
+++ sources/inverter-mqtt/mqtt-init.sh
@@ -23,11 +23,12 @@ registerTopic () {
         -t ""$MQTT_TOPIC"/sensor/"$MQTT_DEVICENAME"_"$MQTT_SERIAL"/$1/config" \
         -r \
         -m "{
-            \"name\": \"$1"\",
+            \"name\": \"$5"\",
             \"uniq_id\": \""$MQTT_SERIAL"_$1\",
             \"device\": { \"ids\": \""$MQTT_SERIAL"\", \"mf\": \""$MQTT_MANUFACTURER"\", \"mdl\": \""$MQTT_MODEL"\", \"name\": \""$MQTT_DEVICENAME"\", \"sw\": \""$MQTT_VER"\"},
             \"state_topic\": \""$MQTT_TOPIC"/sensor/"$MQTT_DEVICENAME"_"$MQTT_SERIAL"/$1\",
             \"state_class\": \"measurement\",
+            \"device_class\": \"$4\",
             \"unit_of_measurement\": \"$2\",
             \"icon\": \"mdi:$3\"
         }"
@@ -52,7 +53,7 @@ registerEnergyTopic () {
         -t ""$MQTT_TOPIC"/sensor/"$MQTT_DEVICENAME"_"$MQTT_SERIAL"/$1/config" \
         -r \
         -m "{
-            \"name\": \"$1"\",
+            \"name\": \"$5"\",
             \"uniq_id\": \""$MQTT_SERIAL"_$1\",
             \"device\": { \"ids\": \""$MQTT_SERIAL"\", \"mf\": \""$MQTT_MANUFACTURER"\", \"mdl\": \""$MQTT_MODEL"\", \"name\": \""$MQTT_DEVICENAME"\", \"sw\": \""$MQTT_VER"\"},
             \"state_topic\": \""$MQTT_TOPIC"/sensor/"$MQTT_DEVICENAME"_"$MQTT_SERIAL"/$1\",
@@ -72,11 +73,11 @@ registerModeTopic () {
         -t ""$MQTT_TOPIC"/sensor/"$MQTT_DEVICENAME"_"$MQTT_SERIAL"/$1/config" \
         -r \
         -m "{
-            \"name\": \"$1"\",
+            \"name\": \"$5"\",
             \"uniq_id\": \""$MQTT_SERIAL"_$1\",
             \"device\": { \"ids\": \""$MQTT_SERIAL"\", \"mf\": \""$MQTT_MANUFACTURER"\", \"mdl\": \""$MQTT_MODEL"\", \"name\": \""$MQTT_DEVICENAME"\", \"sw\": \""$MQTT_VER"\"},
             \"state_topic\": \""$MQTT_TOPIC"/sensor/"$MQTT_DEVICENAME"_"$MQTT_SERIAL"/$1\",
-            \"icon\": \"mdi:$2\"
+            \"icon\": \"mdi:$3\"
         }"
 }
 registerInverterRawCMD () {
@@ -96,38 +97,39 @@ registerInverterRawCMD () {
             }"
 }
 
-registerTopic "AC_charge_on" "" "power" "none"
-registerTopic "AC_grid_frequency" "Hz" "current-ac" "frequency"
-registerTopic "AC_grid_voltage" "V" "power-plug" "voltage"
-registerTopic "AC_out_frequency" "Hz" "current-ac" "frequency"
-registerTopic "AC_out_voltage" "V" "power-plug" "voltage"
-registerTopic "Battery_bulk_voltage" "V" "current-dc" "voltage"
-registerTopic "Battery_capacity" "%" "battery-outline" "battery"
-registerTopic "Battery_charge_current" "A" "current-dc" "current"
-registerTopic "Battery_discharge_current" "A" "current-dc" "current"
-registerTopic "Battery_float_voltage" "V" "current-dc" "voltage"
-registerTopic "Battery_recharge_voltage" "V" "current-dc" "voltage"
-registerTopic "Battery_redischarge_voltage" "V" "battery-negative" "voltage"
-registerTopic "Battery_under_voltage" "V" "current-dc" "voltage"
-registerTopic "Battery_voltage" "V" "battery-outline" "voltage"
-registerTopic "Bus_voltage" "V" "details" "voltage"
-registerTopic "Charger_source_priority" "" "solar-power" "none"
-registerTopic "Heatsink_temperature" "°C" "details" "temperature"
-registerTopic "Load_pct" "%" "brightness-percent" "none"
-registerTopic "Load_status_on" "" "power" "none"
-registerTopic "Load_va" "VA" "chart-bell-curve" "apparent_power"
-registerTopic "Load_watt" "W" "chart-bell-curve" "power"
-registerEnergyTopic "Load_watthour" "Wh" "chart-bell-curve" "energy"
-registerTopic "Max_charge_current" "A" "current-ac" "current"
-registerTopic "Max_grid_charge_current" "A" "current-ac" "current"
-registerModeTopic "Inverter_mode" "solar-power" # 1 = Power_On, 2 = Standby, 3 = Line, 4 = Battery, 5 = Fault, 6 = Power_Saving, 7 = Unknown
-registerTopic "Out_source_priority" "" "grid" "none"
-registerTopic "PV_in_current" "A" "solar-panel-large" "current"
-registerTopic "PV_in_voltage" "V" "solar-panel-large" "voltage"
-registerEnergyTopic "PV_in_watthour" "Wh" "solar-panel-large" "energy"
-registerTopic "PV_in_watts" "W" "solar-panel-large" "power"
-registerTopic "SCC_charge_on" "" "power" "none"
-registerTopic "SCC_voltage" "V" "current-dc" "voltage"
+#              $1code    $2unit of measure  $3mdi $4class   $5name
+registerTopic "AC_charge_on" "" "power" "none" "AC charge on"
+registerTopic "AC_grid_frequency" "Hz" "current-ac" "frequency" "AC grid frequency"
+registerTopic "AC_grid_voltage" "V" "power-plug" "voltage" "AC grid voltage"
+registerTopic "AC_out_frequency" "Hz" "current-ac" "frequency" "AC out frequency"
+registerTopic "AC_out_voltage" "V" "power-plug" "voltage" "AC out voltage"
+registerTopic "Battery_bulk_voltage" "V" "current-dc" "voltage" "Battery bulk voltage"
+registerTopic "Battery_capacity" "%" "battery-outline" "battery" "Battery capacity"
+registerTopic "Battery_charge_current" "A" "current-dc" "current" "Battery charge current"
+registerTopic "Battery_discharge_current" "A" "current-dc" "current" "Battery discharge current"
+registerTopic "Battery_float_voltage" "V" "current-dc" "voltage" "Battery float voltage"
+registerTopic "Battery_recharge_voltage" "V" "current-dc" "voltage" "Battery recharge voltage"
+registerTopic "Battery_redischarge_voltage" "V" "battery-negative" "voltage" "Battery redischarge voltage"
+registerTopic "Battery_under_voltage" "V" "current-dc" "voltage" "Battery under voltage"
+registerTopic "Battery_voltage" "V" "battery-outline" "voltage" "Battery voltage"
+registerTopic "Bus_voltage" "V" "details" "voltage" "Bus voltage"
+registerTopic "Charger_source_priority" "" "solar-power" "none" "Charger source priority"
+registerTopic "Heatsink_temperature" "°C" "details" "temperature" "Heatsink temperature"
+registerTopic "Load_pct" "%" "brightness-percent" "none" "Load pct"
+registerTopic "Load_status_on" "" "power" "none" "Load status on"
+registerTopic "Load_va" "VA" "chart-bell-curve" "apparent_power" "Load va"
+registerTopic "Load_watt" "W" "chart-bell-curve" "power" "Load watt"
+registerEnergyTopic "Load_watthour" "Wh" "chart-bell-curve" "energy" "Load watthour"
+registerTopic "Max_charge_current" "A" "current-ac" "current" "Max charge current"
+registerTopic "Max_grid_charge_current" "A" "current-ac" "current" "Max grid charge current"
+registerModeTopic "Inverter_mode" "" "solar-power" "none" "Inverter mode" # 1 = Power_On, 2 = Standby, 3 = Line, 4 = Battery, 5 = Fault, 6 = Power_Saving, 7 = Unknown
+registerTopic "Out_source_priority" "" "grid" "none" "Out source priority"
+registerTopic "PV_in_current" "A" "solar-panel-large" "current" "PV in current"
+registerTopic "PV_in_voltage" "V" "solar-panel-large" "voltage" "PV in voltage"
+registerEnergyTopic "PV_in_watthour" "Wh" "solar-panel-large" "energy" "PV in watthour"
+registerTopic "PV_in_watts" "W" "solar-panel-large" "power" "PV in watts"
+registerTopic "SCC_charge_on" "" "power" "none" "SCC charge on"
+registerTopic "SCC_voltage" "V" "current-dc" "voltage" "SCC voltage"
 
 # Add in a separate topic so we can send raw commands from assistant back to the inverter via MQTT (such as changing power modes etc)...
 registerInverterRawCMD
--- sources/inverter-mqtt/mqtt-subscriber.sh
+++ sources/inverter-mqtt/mqtt-subscriber.sh
@@ -14,4 +14,4 @@ do
     echo "Incoming request send: [$rawcmd] to inverter."
     /opt/inverter-cli/bin/inverter_poller -r $rawcmd;
 
-done < <(mosquitto_sub -h $MQTT_SERVER -p $MQTT_PORT -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -i ""$MQTT_DEVICENAME"_"$MQTT_SERIAL"" -t "$MQTT_TOPIC/sensor/$MQTT_DEVICENAME" -q 1)
+done < <(mosquitto_sub -h $MQTT_SERVER -p $MQTT_PORT -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -i ""$MQTT_DEVICENAME"_"$MQTT_SERIAL"" -t "$MQTT_TOPIC/sensor/$MQTT_DEVICENAME/COMMANDS" -q 1)
