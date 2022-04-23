c31bca9e3c2bd20fdfd0e135fb6bbf8ab8948048
Catalin Bordan <catalinbordan@gmail.com>
2022-04-23 11:13:26 +0300

    Update main.cpp

--- sources/inverter-cli/main.cpp
+++ sources/inverter-cli/main.cpp
@@ -91,8 +91,8 @@ void getSettingsFile(string filename) {
                     attemptAddSetting(&ampfactor, linepart2);
                 else if(linepart1 == "watt_factor")
                     attemptAddSetting(&wattfactor, linepart2);
-                else if(linepart1 == "watt_factor")
-                    attemptAddSetting(&wattfactor, linepart2);
+                // else if(linepart1 == "watt_factor")
+                //     attemptAddSetting(&wattfactor, linepart2);
                 else if(linepart1 == "qpiri")
                     attemptAddSetting(&qpiri, linepart2);
                 else if(linepart1 == "qpiws")
