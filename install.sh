fa7e9c587924e97f24ae322c02f8d306edc9316a
Nick <73203924+technickfreak2000@users.noreply.github.com>
2024-08-31 01:05:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ case $yn in
 	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
- 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+ 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
         echo ""
         if ! [ "$DEBUG" = true ] ; then
             for i in {3..1};do echo -n "$i." && sleep 1; done
