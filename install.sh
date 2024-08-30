c55db534089ad62295ec9d03c1f620e2a8b5163a
Nick <73203924+technickfreak2000@users.noreply.github.com>
2024-08-31 01:04:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,7 +58,18 @@ fi
 case $yn in 
 	[yY] ) echo "Ok, installing docker, docker-compose and nano";
         sudo apt update 
-        sudo apt install docker.io docker-compose nano -y
+        sudo apt install ca-certificates curl nano -y
+	sudo install -m 0755 -d /etc/apt/keyrings
+ 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+	
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
+ 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         echo ""
         if ! [ "$DEBUG" = true ] ; then
             for i in {3..1};do echo -n "$i." && sleep 1; done
