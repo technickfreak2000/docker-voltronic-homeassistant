06b11f011e904e8ad82957f81f6d2ac9b02e1d98
technickfreak2000 <admin@technickfreak.com>
2024-08-31 15:12:24 +0200

    maybe

--- sources/inverter-cli/inverter.cpp
+++ sources/inverter-cli/inverter.cpp
@@ -161,7 +161,7 @@ void cInverter::GetQPGSn(QPGSn *qpgsn)
   while (query(combined_query) &&
          ((strncmp((char *)&buf[1], "NAK", 3) != 0) || (strncmp((char *)&buf[1], "0", 1) != 0)))
   {
-    printf("HELP ME UNDERSTAND:  %c %c %c\n", &buf[1], &buf[2], &buf[3]);
+    printf("HELP ME UNDERSTAND:  %c %c %c\n", buf[1], buf[2], buf[3]);
     sprintf(combined_query, "QPGS%ld", query_number);
     query_number++;
     current->next = (QPGSn *)malloc(sizeof(QPGSn));
